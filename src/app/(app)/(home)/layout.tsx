05fee071b80d7d57ba1d95c5807b3a2ca4319226
unknown <ocoten321@gmail.com>
2025-06-19 11:24:50 +0700

    add payload cms

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+
 import {Navbar} from "@/components/Navbar";
 import {Footer} from "@/components/Footer";
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Link from "next/link";
 import {cn} from "@/lib/utils";
 import {Poppins} from "next/font/google";
